f4d034d42ed2629544f1493d160647825282a031
Jakob <jakob3067.tietgen@gmail.com>
2026-05-18 09:52:55 +0200

    inertia værdier anderledes

--- cpp/qube_types.h
+++ cpp/qube_types.h
@@ -48,12 +48,14 @@ struct QubeState {
 struct QubeParams {
     double Rm   = 7.5;       // Motor terminal resistance [Ohm] (Qube-Servo 3 user manual Table 2.2)
     double km   = 0.042;     // Motor torque constant = back-EMF constant [N·m/A = V·s/rad]
-    double Jr   = 4.0e-6;    // Rotor (motor hub) moment of inertia [kg·m²]
+    //double Jr   = 4.0e-6;    // Rotor (motor hub) moment of inertia [kg·m²]
+    double Jr   = 6.2e-5; 
     double Lr   = 0.0826;    // Arm length from motor shaft to pendulum pivot [m]
     double Dr   = 0.0;       // Arm viscous damping coefficient [N·m·s/rad]
     double mp   = 0.024;     // Pendulum mass [kg]
     double Lp   = 0.129;     // Pendulum total length (pivot to tip) [m] — TODO: measure! Datasheet says 9.5cm, workbook says 12.9cm
-    double Jp   = 3.33e-5;   // Pendulum moment of inertia about pivot [kg·m²]
+    //double Jp   = 3.33e-5;   // Pendulum moment of inertia about pivot [kg·m²]
+    double Jp   = 1.26e-4; 
     double Dp   = 0.0;       // Pendulum viscous damping coefficient [N·m·s/rad]
     double g    = 9.81;      // Gravitational acceleration [m/s²]
 
